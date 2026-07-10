820140c0bc892cbb79f0aa05ae5c74522899cd06
YMORAN03 <yamilamoran03@gmail.com>
2026-07-09 22:18:00 -0300

    Update soluciones.sql

--- soluciones.sql
+++ soluciones.sql
@@ -7,19 +7,18 @@
 -- Pregunta de negocio: ¿Qué productos del catálogo nunca fueron vendidos?
 -- Mostrá todos los productos y sus ventas asociadas.
 -- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
--- SELECT productos.producto_id,productos.nombre, productos.categoria,
-COUNT(ventas.venta_id) AS total_ventas
+SELECT distinct productos.producto_id, productos.nombre, productos.categoria
 FROM productos
-LEFT JOIN ventas
-ON productos.producto_id = ventas.producto_id
-group by productos.producto_id, productos.nombre, productos.categoria;
+left join ventas
+on productos.producto_id = ventas.producto_id
+where ventas.venta_id is null;
 
 
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos
 -- que no figuran en nuestro catálogo? (posible error de carga de datos)
 -- Los registros huérfanos aparecerán con NULL en las columnas de productos.
--- SELECT ventas.producto_id,ventas.fecha_venta, ventas.venta_id, ventas.cliente_id
+SELECT ventas.producto_id,ventas.fecha_venta, ventas.venta_id, ventas.cliente_id
 FROM productos
 RIGHT JOIN ventas 
 ON productos.producto_id = ventas.producto_id
@@ -29,8 +28,7 @@ where productos.producto_id is null;
 -- Pregunta de negocio: Vista completa de auditoría que muestre
 -- todos los productos y todas las ventas sin perder ninguna fila,
 -- identificando tanto productos sin ventas como ventas sin producto.
-SELECT productos.producto_id, productos.nombre, 
-ventas.venta_id, ventas.fecha_venta , ventas.producto_id 
+SELECT productos.producto_id, productos.nombre, ventas.venta_id, ventas.fecha_venta , ventas.producto_id 
 FROM productos
 FULL OUTER JOIN ventas
 ON productos.producto_id = ventas.producto_id;
